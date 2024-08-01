31af161d580c949b602be96f29e9c86f12b4fff4
EsziL <soos.balint1234@gmail.com>
2024-08-01 13:40:17 +0200

    Fixed some error bugs

--- src/ui/etc/error.cpp
+++ src/ui/etc/error.cpp
@@ -1,10 +1,14 @@
 #include "error.h"
 #include <iostream>
-
-
+#include <queue>
+#include <string>
 
 GtkWidget* gErrorBox = NULL;
 GtkWidget* errorLabel = NULL;
+guint hideErrorTimeoutId = 0;
+std::queue<std::string> errorQueue;
+
+gboolean showNextError(gpointer data);
 
 void errorBoxInit() {
     gErrorBox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
@@ -15,23 +19,53 @@ void errorBoxInit() {
 }
 
 gboolean hideError(gpointer widget) {
-    if (errorLabel != NULL) {
-        gtk_widget_set_visible(gErrorBox, FALSE);
-        gtk_widget_remove_css_class(gErrorBox, "errorBox");
+    gtk_widget_set_visible(gErrorBox, FALSE);
+    gtk_widget_remove_css_class(gErrorBox, "errorBox");
+    hideErrorTimeoutId = 0;
+
+    if (!errorQueue.empty()) g_timeout_add(50, showNextError, NULL);
+    return G_SOURCE_REMOVE;
+}
+
+gboolean addCssClass(gpointer widget) {
+    gtk_widget_add_css_class(GTK_WIDGET(widget), "errorBox");
+    return G_SOURCE_REMOVE;
+}
+
+gboolean showNextError(gpointer data) {
+    if (!errorQueue.empty()) {
+        std::string nextError = errorQueue.front();
+        errorQueue.pop();
+        showError(nextError);
     }
     return G_SOURCE_REMOVE;
 }
 
 void showError(const std::string& message) {
-    if (errorLabel != NULL) {
-        gtk_box_remove(GTK_BOX(gErrorBox), errorLabel);
+    if (gtk_widget_is_visible(gErrorBox)) {
+        errorQueue.push(message);
+        return;
     }
-    gtk_widget_set_visible(gErrorBox, TRUE);
-    errorLabel = gtk_label_new(message.c_str());
-    gtk_box_append(GTK_BOX(gErrorBox), errorLabel);
-    gtk_widget_set_halign(errorLabel, GTK_ALIGN_CENTER);
-    gtk_widget_set_valign(errorLabel, GTK_ALIGN_CENTER);
-    gtk_widget_add_css_class(gErrorBox, "errorBox");
 
-    g_timeout_add(4000, hideError, NULL);
+
+    gtk_widget_set_visible(gErrorBox, TRUE);
+    gtk_widget_remove_css_class(gErrorBox, "errorBox");
+
+    if (errorLabel == NULL) {
+        errorLabel = gtk_label_new(message.c_str());
+        gtk_box_append(GTK_BOX(gErrorBox), errorLabel);
+        gtk_widget_set_halign(errorLabel, GTK_ALIGN_CENTER);
+        gtk_widget_set_valign(errorLabel, GTK_ALIGN_CENTER);
+        gtk_widget_set_hexpand(errorLabel, TRUE);
+        gtk_widget_set_vexpand(errorLabel, TRUE);
+    } else {
+        gtk_label_set_text(GTK_LABEL(errorLabel), message.c_str());
+    }
+
+    g_timeout_add(50, addCssClass, gErrorBox);
+
+    if (hideErrorTimeoutId != 0) g_source_remove(hideErrorTimeoutId);
+
+
+    hideErrorTimeoutId = g_timeout_add(4000, hideError, NULL);
 }
